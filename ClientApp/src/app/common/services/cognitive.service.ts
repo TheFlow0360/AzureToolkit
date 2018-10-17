95aec82c4c8346752da5078f354d372cf4888c9b
Florian Koch <floriankochde@arcor.de>
2018-10-17 14:49:12 +0200

    + image analysis

--- ClientApp/src/app/common/services/cognitive.service.ts
+++ ClientApp/src/app/common/services/cognitive.service.ts
@@ -4,10 +4,14 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import { AzureHttpClient } from './azureHttpClient';
 import { BingSearchResponse } from '../models/bingSearchResponse';
+import { ComputerVisionRequest, ComputerVisionResponse } from '../models/computerVisionResponse';
 @Injectable()
 export class CognitiveService {
     bingSearchAPIKey = '1a3ff31b3f024b39bc009d5e9e12e522';
+    computerVisionAPIKey = '179d5ead22e14d558fccc423e04179fd';
+
     constructor(private http: AzureHttpClient) { }
+
     searchImages(searchTerm: string): Observable<BingSearchResponse> {
         return this.http.get(
                 'https://api.cognitive.microsoft.com/bing/v7.0/images/search?safeSearch=Off&q=' + searchTerm,
@@ -15,6 +19,15 @@ export class CognitiveService {
             .map(response => response.json() as BingSearchResponse)
             .catch(this.handleError);
     }
+
+    analyzeImage(request: ComputerVisionRequest): Observable<ComputerVisionResponse> {
+        return this.http.post(
+                'https://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags',
+                this.computerVisionAPIKey, request)
+            .map(response => response.json() as ComputerVisionResponse)
+            .catch(this.handleError);
+    }
+
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
--- ClientApp/src/app/search/search.component.ts
+++ ClientApp/src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ImageResult } from '../common/models/bingSearchResponse';
 import { CognitiveService } from '../common/services/cognitive.service';
+import { ComputerVisionResponse, ComputerVisionRequest } from '../common/models/computerVisionResponse';
 
 @Component({
     selector: 'app-search',
@@ -10,15 +11,30 @@ import { CognitiveService } from '../common/services/cognitive.service';
 export class SearchComponent {
     searchResults: ImageResult[] | null;
     isSearching = false;
+    currentAnalytics: ComputerVisionResponse | null;
+    currentItem: ImageResult | null;
+    isAnalyzing = false;
 
     constructor(private cognitiveService: CognitiveService) { }
 
     search(searchTerm: string) {
         this.searchResults = null;
+        this.currentAnalytics = null;
         this.isSearching = true;
         this.cognitiveService.searchImages(searchTerm).subscribe(result => {
             this.searchResults = result.value;
             this.isSearching = false;
         });
+    }
+
+    analyze(imageResult: ImageResult) {
+        this.currentItem = imageResult;
+        this.currentAnalytics = null;
+        this.isAnalyzing = true;
+        this.cognitiveService.analyzeImage({ url: imageResult.thumbnailUrl } as ComputerVisionRequest).subscribe(result => {
+            this.currentAnalytics = result;
+            this.isAnalyzing = false;
+        });
+        window.scroll(0, 0);
     }
 }
